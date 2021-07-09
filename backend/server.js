c4f59c592718e79b49f3861c1c7137639a11e856
TiffanySims <44296646+TiffanySims@users.noreply.github.com>
2021-07-08 23:17:37 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,27 @@ import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 
+dotenv.config()
+
+connectDB()
+
+const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json())
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/api/config/paypal', (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+)
+
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
@@ -27,6 +48,8 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(notFound)
 app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 
 
 app.listen(
